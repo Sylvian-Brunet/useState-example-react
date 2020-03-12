db13353fdb5491878a8032b93795144e7fc7320f
Sylvian-Brunet <sylvian84@hotmail.fr>
2020-03-12 14:14:08 +0100

    Commit

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Room.css'
 
 const Room = () => {
-  return (
-    <div className="room">La chambre est disponible</div>
-  );
-}
+  const [isLit, setIsLit] = useState(true);
+  const [isLight, setIsLight] = useState(false);
+  const [temperature, setTemperature] = useState(22);
 
-export default Room
+  return (
+    <div className="room">
+        <div className="disponibilite">
+            <div id="status" className={isLit ? 'free' : 'lit'}></div>
+            <span>La chambre est {isLit ? 'disponible' : 'occupée'}</span>
+            <button onClick={() => {
+                setIsLit(prev => !prev);
+                if (isLit == false) {
+                    setIsLight(false);
+                    setTemperature(22);
+                }
+            }}>
+                Changer l'état
+            </button>
+        </div>
+        <div className="lumiere">
+            <div id="status" className={isLight ? 'lighted' : 'off'}></div>
+            <span>La chambre est {isLight ? 'allumé' : 'éteinte'}</span>
+            <button onClick={() => { if(isLit != true) setIsLight(prev => !prev); }}>
+                {isLight ? 'Eteindre' : 'Allumer'}
+            </button>
+        </div>
+        <div className="temperature">
+            <span>température de la chambre : {temperature} °C</span>
+            <button className="temp" onClick={() => { if(isLit != true) setTemperature(prev => prev-1); }}> - </button>
+            <button className="temp" onClick={() => { if(isLit != true) setTemperature(prev => prev+1); }}> + </button>
+        </div>
+    </div>
+  );
+};
+
+export default Room;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Room from './components/Room';
 
-ReactDOM.render(<Room/>, document.querySelector('#root'));
+ReactDOM.render(<Room/>, document.querySelector('#room1'));
+ReactDOM.render(<Room/>, document.querySelector('#room2'));
